71a99784d9f0c03eae723cf0f5e816f8ddd580aa
firefly12334 <yuqi2005lyq@gmail.com>
2024-07-28 20:58:00 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,36 +4,36 @@
 using namespace std;
 
 string addition(string num1, string num2) {
-    ssize_t l1 = num1.length()-1, l2 = num2.length()-1;
+    ssize_t l1 = num1.length() - 1, l2 = num2.length() - 1;
     string num3;
-    bool over= false;
-    if(l1<l2){
-        swap(num1,num2);
-        swap(l1,l2);
+    bool over = false;
+    if (l1 < l2) {
+        swap(num1, num2);
+        swap(l1, l2);
     }
-    while (l2>=0) {
-        num3.push_back((num1[l1]-'0'+num2[l2]-'0'+over)%10+'0');
+    while (l2 >= 0) {
+        num3.push_back((num1[l1] - '0' + num2[l2] - '0' + over) % 10 + '0');
 
-        if(num1[l1]-'0'+num2[l2]-'0'+over>=10){
-            over= true;
-        }else{
-            over= false;
+        if (num1[l1] - '0' + num2[l2] - '0' + over >= 10) {
+            over = true;
+        } else {
+            over = false;
         }
         l1--;
         l2--;
     }
 
-    while(l1>=0){
-        num3.push_back((num1[l1]-'0'+over)%10+'0');
-        if(num1[l1]-'0'+over>=10){
-            over= true;
-        }else{
-            over= false;
+    while (l1 >= 0) {
+        num3.push_back((num1[l1] - '0' + over) % 10 + '0');
+        if (num1[l1] - '0' + over >= 10) {
+            over = true;
+        } else {
+            over = false;
         }
         l1--;
     }
 
-    if(over){
+    if (over) {
         num3.push_back('1');
     }
 
@@ -42,7 +42,71 @@ string addition(string num1, string num2) {
     return num3;
 }
 
+string subtraction(string num1, string num2) {
+    ssize_t l1 = num1.length() - 1, l2 = num2.length() - 1,l3;
+    string num3;
+    bool over = false,flag= true;
+
+    if(num1==num2){
+        return "0";
+    }
+
+    if (l1 < l2) {
+        swap(num1, num2);
+        swap(l1, l2);
+        flag= false;
+    }
+
+    while (l2 >= 0) {
+        if (num1[l1] >= num2[l2]) {
+            num3.push_back((num1[l1] - '0' - num2[l2] + '0' - over) + '0');
+            over= false;
+        } else {
+            num3.push_back((num1[l1] - '0' + 10 - num2[l2] + '0' - over) + '0');
+            over = true;
+        }
+        l1--;
+        l2--;
+    }
+
+    while (l1 >= 0) {
+        if(over){
+            if(num1[l1]=='0'){
+                over= true;
+                num3.push_back((num1[l1] - '0' + 10 - over)  + '0');
+            }else{
+                num3.push_back((num1[l1] - '0' - over) + '0');
+                over= false;
+            }
+        }else{
+            num3.push_back((num1[l1] - '0' )  + '0');
+        }
+        l1--;
+    }
+
+    if(over){
+        num3.pop_back();
+    }
+
+    l3=num3.length()-1;
+    while(num3[l3]-'0'==0){
+        num3.pop_back();
+        l3--;
+    }
+
+    if(!flag){
+        num3.push_back('-');
+    }
+
+    reverse(num3.begin(), num3.end());
+
+    return num3;
+
+}
+
 int main() {
-    cout<<addition("12345675","0")<<endl;
+    cout << subtraction("100", "100") << endl;
+    cout << subtraction("100", "1") << endl;
+    cout << subtraction("1", "100") << endl;
     return 0;
 }
